62a64d256f091021452314d48f9b2a88e080bd4e
yekyung2 <skybluee2014@gmail.com>
2020-11-05 19:29:52 +0900

    add more classes into rooms.models.py

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,6 +4,11 @@ from . import models
 # Register your models here.
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
