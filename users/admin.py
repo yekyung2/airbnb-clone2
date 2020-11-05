64abceeda457253f25692421d0eb8616704b6652
yekyung2 <skybluee2014@gmail.com>
2020-11-05 16:18:44 +0900

    add more field in user model and add list display to customuseradmin

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ from . import models
 # Register your models here.
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("username", "gender", "language", "currency", "superhost")
--- users/models.py
+++ users/models.py
@@ -4,4 +4,36 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
 
-    pass
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = [
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    ]
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = [(LANGUAGE_KOREAN, "Korean"), (LANGUAGE_ENGLISH, "English")]
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = [(CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD")]
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
