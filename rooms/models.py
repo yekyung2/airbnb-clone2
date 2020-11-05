22a7a72f38fd3f35abf7331930294fb6371bb8d4
yekyung2 <skybluee2014@gmail.com>
2020-11-05 18:45:12 +0900

    add abstract Item class and roomtype class to rooms.models.py

--- rooms/models.py
+++ rooms/models.py
@@ -3,7 +3,25 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definition"""
@@ -22,6 +40,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
 
     def __str__(self):
         return self.name
