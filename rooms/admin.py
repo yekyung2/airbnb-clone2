b0bfbaf0f1493988b9ddd4928b36c4be27ed813d
yekyung2 <skybluee2014@gmail.com>
2020-11-10 14:51:21 +0900

    add fieldsets to room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,6 +17,20 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
         "country",
@@ -34,11 +48,25 @@ class RoomAdmin(admin.ModelAdmin):
 
     list_filter = (
         "instant_book",
+        "room_type",
+        "host__superhost",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     )
 
-    search_fields = ("city",)
+    search_fields = (
+        "city",
+        "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
 
 
 @admin.register(models.Photo)
