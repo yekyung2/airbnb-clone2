46707e9c08066c970cd8e204824533226f14bf26
yekyung2 <skybluee2014@gmail.com>
2020-11-13 17:53:27 +0900

    add photo downloads folders

--- users/models.py
+++ users/models.py
@@ -24,7 +24,7 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = [(CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD")]
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
