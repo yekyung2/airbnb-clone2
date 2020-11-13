d81a7014b0c866f52ccdd33b6afecdc896b8e1c7
yekyung2 <skybluee2014@gmail.com>
2020-11-13 12:17:42 +0900

    add in_progress and is_finished to reservation admin.py

--- reservations/admin.py
+++ reservations/admin.py
@@ -9,6 +9,12 @@ class ReservationAdmin(admin.ModelAdmin):
 
     list_display = (
         "room",
+        "status",
         "check_in",
         "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
     )
+
+    list_filter = ("status",)
