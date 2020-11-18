0a0a6ea9cc7ac672ebaa374f96d50412d0261305
yekyung2 <skybluee2014@gmail.com>
2020-11-18 22:13:59 +0900

    edit handle

---
+++ rooms/management/commands/loveyou.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = "This command tells me that he loves me"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--times", help="How many times do you want me to tell you that I love you?"
+        )
+
+    def handle(self, *args, **options):
+        times = options.get("times")
+        print("I love you")
