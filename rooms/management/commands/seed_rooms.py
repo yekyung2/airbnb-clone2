9337507ffd1c433ee3b52bbd547dce7144911c82
yekyung2 <skybluee2014@gmail.com>
2020-11-23 22:19:24 +0900

    add seed_rooms to rooms management command

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,20 +1,40 @@
+import random
 from django.core.management.base import BaseCommand
-from rooms.models import Facility
+from django_seed import Seed
+from rooms import models as room_models
+from users import models as user_models
 
 
 class Command(BaseCommand):
 
-    # help = "This command creates facilities"
+    help = "This command creates users"
 
-    # def handle(self, *args, **options):
-    #     facilities = [
-    #         "Private entrance",
-    #         "Paid parking on premises",
-    #         "Paid parking off premises",
-    #         "Elevator",
-    #         "Parking",
-    #         "Gym",
-    #     ]
-    #     for f in facilities:
-    #         Facility.objects.create(name=f)
-    #     self.stdout.write(self.style.SUCCESS(f"{len(facilities)} facilities created!"))
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            default=2,
+            type=int,
+            help="How many rooms do you want me to create?",
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()
+        room_types = room_models.RoomType.objects.all()
+        seeder.add_entity(
+            room_models.Room,
+            number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_types),
+                "guests": lambda x: random.randint(1, 20),
+                "price": lambda x: random.randint(1, 300),
+                "beds": lambda x: random.randint(1, 5),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 5),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"{number} room created!"))
