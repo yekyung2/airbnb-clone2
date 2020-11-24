e74855b56cbed2547686d7d54d227984d925a7f3
yekyung2 <skybluee2014@gmail.com>
2020-11-25 01:54:44 +0900

    make a test of views

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def all_rooms(request):
-    pass
+    # print(dir(request))
+    now = datetime.now()
+    return HttpResponse(content=f"<h1>hello {now} </h1>")
