96dd6dcafc2a12c564a4ee86dcba504959fc296c
yekyung2 <skybluee2014@gmail.com>
2020-11-25 01:59:17 +0900

    add render request to rooms views

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,5 @@
-from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def all_rooms(request):
-    # print(dir(request))
-    now = datetime.now()
-    return HttpResponse(content=f"<h1>hello {now} </h1>")
+    return render(request, "all_rooms")
